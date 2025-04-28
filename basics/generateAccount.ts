154d6d7062b1f40f43f379ee41fc34aa4a5e63a2
dudleyneedham <dudleyneedham1993@gmail.com>
2025-04-28 14:48:38 +0200

    feat: adding the get submittable acconut

--- basics/generateAccount.ts
+++ basics/generateAccount.ts
@@ -4,15 +4,20 @@ import type { MultibaseKeyPair } from '@kiltprotocol/types'
 interface GeneratedAccounts {
   issuerAccount: MultibaseKeyPair
   holderAccount: MultibaseKeyPair
+  getSubmittableAccount: MultibaseKeyPair
 }
 
 export function generateAccounts(): GeneratedAccounts {
   const issuerAccount = Kilt.generateKeypair({ type: 'ed25519' })
   const holderAccount = Kilt.generateKeypair({ type: 'ed25519' })
+  const getSubmittableAccount = Kilt.generateKeypair({ type: 'ed25519' })
 
   console.log('keypair generation complete')
   console.log(`ISSUER_ACCOUNT_ADDRESS=${issuerAccount.publicKeyMultibase}`)
   console.log(`HOLDER_ACCOUNT_ADDRESS=${holderAccount.publicKeyMultibase}`)
+  console.log(
+    `GET_SUBMITTABLE_ACCOUNT_ADDRESS=${getSubmittableAccount.publicKeyMultibase}`
+  )
 
-  return { issuerAccount, holderAccount }
+  return { issuerAccount, holderAccount, getSubmittableAccount }
 }
