3bbbda9937b5edf2311ead6c8d270f8a2017b633
dudleyneedham <dudleyneedham1993@gmail.com>
2025-04-28 13:30:23 +0200

    chore: switch u8a of faucet to seed phrase

--- basics/test.ts
+++ basics/test.ts
@@ -12,17 +12,10 @@ async function runAll(): Promise<void> {
 
   console.log('connected')
 
-  const faucet = {
-    publicKey: new Uint8Array([
-      238, 93, 102, 137, 215, 142, 38, 187, 91, 53, 176, 68, 23, 64, 160, 101,
-      199, 189, 142, 253, 209, 193, 84, 34, 7, 92, 63, 43, 32, 33, 181, 210,
-    ]),
-    secretKey: new Uint8Array([
-      205, 253, 96, 36, 210, 176, 235, 162, 125, 84, 204, 146, 164, 76, 217,
-      166, 39, 198, 155, 45, 189, 161, 94, 215, 229, 128, 133, 66, 81, 25, 174,
-      3,
-    ]),
-  }
+  const faucetMnemonic =
+    'receive clutch item involve chaos clutch furnace arrest claw isolate okay together'
+
+  const faucet = Kilt.generateKeypair({ seed: faucetMnemonic })
 
   const [submitter] = (await Kilt.getSignersForKeypair({
     keypair: faucet,
