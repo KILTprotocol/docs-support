b3f54b2bc4cda0314ae984d6d9f30c680af7b481
dudleyneedham <dudleyneedham1993@gmail.com>
2025-04-28 14:48:16 +0200

    feat: creating the submittable getter and handler

---
+++ basics/getSubmittable.ts
@@ -0,0 +1,48 @@
+import { ConfigService, DidHelpers } from '@kiltprotocol/sdk-js'
+import { Blockchain } from '@kiltprotocol/chain-helpers'
+
+import {
+  SignerInterface,
+  KiltAddress,
+  HexString,
+  MultibaseKeyPair,
+} from '@kiltprotocol/types'
+
+export async function getSubmittable(
+  authenticationKeyPair: MultibaseKeyPair,
+  submitterAddress: KiltAddress
+): Promise<HexString> {
+  const api = ConfigService.get('api')
+
+  const transactionHandler = DidHelpers.createDid({
+    api,
+    signers: [authenticationKeyPair],
+    submitter: submitterAddress,
+    fromPublicKey: authenticationKeyPair.publicKeyMultibase,
+  })
+
+  const didDocumentTransactionResultTx =
+    await transactionHandler.getSubmittable({
+      signSubmittable: false,
+    })
+
+  return didDocumentTransactionResultTx.txHex
+}
+
+export async function handleSubmittable(
+  submittableHex: HexString,
+  submitter: SignerInterface<'Ed25519', KiltAddress>
+): Promise<void> {
+  const api = ConfigService.get('api')
+
+  console.log(submittableHex)
+
+  const result = await Blockchain.signAndSubmitTx(
+    api.tx(submittableHex),
+    submitter
+  )
+
+  console.log(result.isCompleted)
+
+  console.log(result.toHuman())
+}
